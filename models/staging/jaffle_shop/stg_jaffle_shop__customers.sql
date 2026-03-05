b816d573319b250a86acc42905292cadc8150d61
JeroenAtCleHor <jeroen@cleverhorizons.nl>
2026-03-05 18:24:08 +0100

    staging changed with results from code generation

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,28 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
+-- select
+--     id as customer_id,
+--     first_name,
+--     last_name
 
-from {{ source('jaffle_shop', 'customers') }}
+-- from {{ source('jaffle_shop', 'customers') }}
+
+-- following code is generated with codegen: 
+-- dbt run-operation generate_base_model --args '{"source_name": "jaffle_shop", "table_name": "customers"}'
+
+with source as (
+
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from source
+
+)
+
+select * from renamed
